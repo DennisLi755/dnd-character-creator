b4cce92f3eee34bb61ca7e56ffb35474263e1115
Dennis <dennisli755@gmail.com>
2023-12-27 18:05:07 -0500

    finished up backend views and started frontend

--- backend/character_app/models.py
+++ backend/character_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from jsonfield import JSONField
+from .validators import validate_name
 
 # Create your models here.
 class Character(models.Model):
@@ -10,6 +11,6 @@ class Character(models.Model):
     char_class = models.CharField(max_length=255)
     health = models.IntegerField(default=1)
     movement = models.IntegerField(default=30)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, validators=[validate_name])
     race = models.CharField(max_length=255)
     ability = JSONField()
--- backend/character_app/views.py
+++ backend/character_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from .models import Character
+from .serializers import CharacterSerializer
+# Import both APIView and Response from DRF
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class All_characters(APIView):
+    # Just like we said before we only want this information available for GET requests therefore we have to place this logic under a GET method. DRF will recognize the `get` method and trigger that method every time a GET request is sent
+    def get(self, request):
+        character = CharacterSerializer(Character.objects.order_by('name'), many=True)
+        # Under response we don't necessarily need to send information in JSON format instead DRF will format our response and make it acceptable for Front-End frameworks
+        return Response(character.data)
